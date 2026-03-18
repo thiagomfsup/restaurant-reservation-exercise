e9d1381c521bc8eb64e7460ceb60a79ef538f409
López Núñez <pulz@gft.com>
2026-03-18 15:17:34 +0100

    feat: phase 1 - restaurant management with TDD and repository pattern

--- src/main/java/com/gft/domain/ReservationNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.gft.domain;
-
-public class ReservationNotFoundException extends RuntimeException {
-
-    public ReservationNotFoundException(String reservationId) {
-        super("No existe ninguna reserva con id: " + reservationId);
-    }
-}
--- src/main/java/com/gft/domain/ReservationNotValidException.java
+++
@@ -1,8 +0,0 @@
-package com.gft.domain;
-
-public class ReservationNotValidException extends RuntimeException {
-
-    public ReservationNotValidException(String message) {
-        super(message);
-    }
-}
