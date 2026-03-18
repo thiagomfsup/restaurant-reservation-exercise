a2d7f0230580210a1975d7705b39210be0c01c4e
mion <913mario@gmail.com>
2026-03-18 13:12:03 +0100

    Added class: RestaurantManager

--- src/main/java/RestaurantManager.java
+++ src/main/java/RestaurantManager.java
@@ -1,2 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class RestaurantManager {
+    Set<Restaurant> restaurantsSet;
+
+    public RestaurantManager() {
+        this.restaurantsSet = new HashSet<>();
+    }
+
+
+
+
 }
