20659b140c793708f92f12c56ab34c880254e5dc
mion <913mario@gmail.com>
2026-03-18 13:05:31 +0100

    Added class: RestaurantManager

--- src/main/java/Reservation.java
+++ src/main/java/Reservation.java
@@ -1,2 +1,17 @@
+import java.sql.Time;
+import java.util.Date;
+
 public class Reservation {
+    private int id;
+    private int restaurantId;
+    private String customerName;
+    private String customerPhone;
+    private int partySize;
+    private Date ReservationDateTime;
+
+
+    public Reservation() {
+
+    }
+
 }
--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -1,2 +1,24 @@
 public class Restaurant {
+
+    private int id;
+    private String name;
+    private int capacity;
+    // OPENING HOURS
+    // OPENING DAYS
+
+
+}
+
+enum Day {
+    MONDAY(),
+    TUESDAY(),
+    WEDNESDAY(),
+    THURSDAY(),
+    FRIDAY(),
+    SATURDAY(),
+    SUNDAY();
+
+    Day() {
+
+    }
 }
